8ec6b0000656cd0888a7ea68b05449a42903f0fe
johnny <johnny@topazelectro.ru>
2021-04-30 14:02:52 +0300

    Итератор создается через фабрику

--- home24/src/main/java/su/nepom/Main.java
+++ home24/src/main/java/su/nepom/Main.java
@@ -3,6 +3,7 @@ package su.nepom;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.util.function.Supplier;
 
 public class Main {
     static void generate(OutputStream stream, int count, Iterator iterator) {
@@ -10,6 +11,10 @@ public class Main {
             for (; count > 0 && iterator.hasNext(); --count)
                 pw.println(iterator.next());
         }
+    }
+
+    static Supplier<Iterator> getIteratorFactory(boolean reverse, int count) {
+        return () -> reverse ? new ReverseFibonacciIterator(count) : new FibonacciIterator();
     }
 
     public static void main(String[] args) throws Exception {
@@ -20,8 +25,10 @@ public class Main {
         var count = Integer.parseInt(args[1]);
         var reverse = Integer.parseInt(args[2]) == 1;
 
+        var factory = getIteratorFactory(reverse, count);
+
         try (var out = new FileOutputStream(file)) {
-            generate(out, count, reverse ? new ReverseFibonacciIterator(count) : new FibonacciIterator());
+            generate(out, count, factory.get());
         }
     }
 }
